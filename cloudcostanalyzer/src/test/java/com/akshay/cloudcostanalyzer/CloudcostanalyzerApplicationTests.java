ad8d3e94fabfdd9cdcc3b56a8566790bd7b3ebe4
akshaya.k <akshaya.k@solize.com>
2026-05-17 20:16:11 +0530

    Initial Spring Boot setup with MySQL integration

---
+++ cloudcostanalyzer/src/test/java/com/akshay/cloudcostanalyzer/CloudcostanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.cloudcostanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcostanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/akshay/cloudcostanalyzer/CloudcostanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.cloudcostanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcostanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcostanalyzerApplication.class, args);
+	}
+
+}
